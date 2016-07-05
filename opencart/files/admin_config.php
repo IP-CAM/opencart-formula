40948ab7fb63cf25aee18763d7cb21be2964d61c
Fintan MacMahon <fintan.macmahon@gmail.com>
2016-07-05 15:08:33 +0100

    Updated paths to point to the admin folder.

--- opencart/files/admin_config.php
+++ opencart/files/admin_config.php
@@ -1,16 +1,19 @@
 <?php
 
 // HTTP
-define('HTTP_SERVER', '{{ url }}');
+define('HTTP_SERVER', '{{ url }}/admin/');
+define('HTTP_CATALOG', '{{ url }}/');
 
 // HTTPS
-define('HTTPS_SERVER', '{{ url }}');
-define('HTTPS_IMAGE', '{{ url }}');
+define('HTTPS_SERVER', '{{ url }}/admin/');
+define('HTTPS_CATALOG', '{{ url }}/');
+define('HTTPS_IMAGE', '{{ url }}/');
+
 // DIR
-define('DIR_APPLICATION', '{{ path }}/catalog/');
+define('DIR_APPLICATION', '{{ path }}/admin/');
 define('DIR_SYSTEM', '{{ path }}/system/');
-define('DIR_LANGUAGE', '{{ path }}/catalog/language/');
-define('DIR_TEMPLATE', '{{ path }}/catalog/view/theme/');
+define('DIR_LANGUAGE', '{{ path }}/admin/language/');
+define('DIR_TEMPLATE', '{{ path }}/admin/view/template/');
 define('DIR_CONFIG', '{{ path }}/system/config/');
 define('DIR_IMAGE', '{{ path }}/image/');
 define('DIR_CACHE', '{{ path }}/system/storage/cache/');
@@ -18,6 +21,7 @@ define('DIR_DOWNLOAD', '{{ path }}/system/storage/download/');
 define('DIR_LOGS', '{{ path }}/system/storage/logs/');
 define('DIR_MODIFICATION', '{{ path }}/system/storage/modification/');
 define('DIR_UPLOAD', '{{ path }}/system/storage/upload/');
+define('DIR_CATALOG', '{{ path }}/catalog/');
 
 // DB
 define('DB_DRIVER', '{{ dbdriver }}');
