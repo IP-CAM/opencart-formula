a2bd5ce13df17f4f1e5e000d622824137494ac33
Fintan MacMahon <fintan.macmahon@gmail.com>
2016-07-05 15:04:00 +0100

    Removed public folder from path.

--- opencart/files/config.php
+++ opencart/files/config.php
@@ -7,17 +7,17 @@ define('HTTP_SERVER', '{{ url }}');
 define('HTTPS_SERVER', '{{ url }}');
 define('HTTPS_IMAGE', '{{ url }}');
 // DIR
-define('DIR_APPLICATION', '{{ path }}/public/catalog/');
-define('DIR_SYSTEM', '{{ path }}/public/system/');
-define('DIR_LANGUAGE', '{{ path }}/public/catalog/language/');
-define('DIR_TEMPLATE', '{{ path }}/public/catalog/view/theme/');
-define('DIR_CONFIG', '{{ path }}/public/system/config/');
-define('DIR_IMAGE', '{{ path }}/public/image/');
-define('DIR_CACHE', '{{ path }}/public/system/storage/cache/');
-define('DIR_DOWNLOAD', '{{ path }}/public/system/storage/download/');
-define('DIR_LOGS', '{{ path }}/public/system/storage/logs/');
-define('DIR_MODIFICATION', '{{ path }}/public/system/storage/modification/');
-define('DIR_UPLOAD', '{{ path }}/public/system/storage/upload/');
+define('DIR_APPLICATION', '{{ path }}/catalog/');
+define('DIR_SYSTEM', '{{ path }}/system/');
+define('DIR_LANGUAGE', '{{ path }}/catalog/language/');
+define('DIR_TEMPLATE', '{{ path }}/catalog/view/theme/');
+define('DIR_CONFIG', '{{ path }}/system/config/');
+define('DIR_IMAGE', '{{ path }}/image/');
+define('DIR_CACHE', '{{ path }}/system/storage/cache/');
+define('DIR_DOWNLOAD', '{{ path }}/system/storage/download/');
+define('DIR_LOGS', '{{ path }}/system/storage/logs/');
+define('DIR_MODIFICATION', '{{ path }}/system/storage/modification/');
+define('DIR_UPLOAD', '{{ path }}/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', '{{ dbdriver }}');
